47cad4048625e0120d3f2a09b827cd63637564ea
tomboloski <ndanumumo93@gmail.com>
2023-10-04 14:36:19 +0300

    day 5

--- src/pages/classes.js
+++ src/pages/classes.js
@@ -42,7 +42,7 @@ function classes() {
       <div className="sm:mt-36 mt-28">
             <div className="bg-white p-2">
 
-                <div className="flex flex-row">
+                <div className="flex flex-row items-center justify-center w-full">
                     <div>
                         <h2 className="font-bold ml-5 text-blue-700 my-2 text-lg">Monthly Premium Packages</h2>
                     </div>
@@ -62,21 +62,30 @@ function classes() {
 
 
 
-                    <div className="bg-gray-300 rounded-md lg:w-1/2 mb-3 p-1 flex flex-row justify-between">
+                    <div className="bg-gray-300 rounded-md lg:w-full mb-3 p-1 flex flex-col justify-center items-center">
                     <h2 className="text-xs font-bold ml-5 text-blue-900 my-2">DERIV ACCOUNT MANAGEMENT</h2>
-                    <button className=" bg-white rounded-sm px-2 text-sm hover:bg-blue-900 hover:text-white" onClick={() => createCheckoutSession('DERIV ACCOUNT MANAGEMENT')}>Apply</button>
+                    <p className="p-2 text-sm font-semibold flex flex-grow items-center justify-center">Deriv account management refers to the process of overseeing and maintaining an individual's or organization's 
+                      trading accounts on the Deriv platform. This includes tasks such as depositing and withdrawing funds, monitoring account balances, 
+                      managing risk through position adjustments, and ensuring compliance with trading regulations.
+                       Effective account management is essential for successful and responsible trading in financial markets.</p>
+                    <p className="p-3 text-lg font-semibold text-blue-900">KSH 25,000</p>
+                    <button className=" bg-blue-500 rounded-sm px-6 py-2 text-sm hover:bg-blue-900 hover:text-white" onClick={() => createCheckoutSession('DERIV ACCOUNT MANAGEMENT')}>Apply</button>
                     </div>
 
 
 
-                    <div className="bg-gray-300 rounded-md lg:w-1/2 mb-5 p-1 flex flex-row justify-between">
+                    <div className="bg-white mt-10 border-2 border-gray-200 rounded-md lg:w-full mb-3 p-1 flex flex-col justify-center items-center">
                     <h2 className="text-xs font-bold ml-5 text-blue-900 my-2">PHYSICAL BINARY CLASSES</h2>
-                    <button className=" bg-white rounded-sm px-2 text-sm hover:bg-blue-900 hover:text-white" onClick={() => createCheckoutSession('PHYSICAL BINARY CLASSES')}>Apply</button>
+                    <p className="p-2 text-sm font-semibold flex flex-grow items-center justify-center">Physical binary classes in Deriv refer to a category of trading options where the outcome is determined by the actual physical occurrence of an event, such as the price of a specific asset reaching a certain level or an economic indicator hitting a predefined mark. Unlike traditional binary options that rely solely on price speculation, physical binary classes involve real-world factors, making them an interesting and potentially more predictable way to trade various financial instruments. These classes offer traders a unique opportunity to engage with market events in a tangible manner.</p>
+                    <p className="p-3 text-lg font-semibold text-blue-900">KSH 20,000</p>
+                    <button className=" bg-blue-500 rounded-sm px-6 py-2 text-sm hover:bg-blue-900 hover:text-white" onClick={() => createCheckoutSession('DERIV ACCOUNT MANAGEMENT')}>Apply</button>
                     </div>
 
-                    <div className="bg-gray-300 rounded-md lg:w-1/2 mb-5 p-1 flex flex-row justify-between">
+                    <div className="bg-gray-300 mt-10 rounded-md lg:w-full mb-3 p-1 flex flex-col justify-center items-center">
                     <h2 className="text-xs font-bold ml-5 text-blue-900 my-2">ONLINE BINARY CLASSES</h2>
-                    <button className=" bg-white rounded-sm px-2 text-sm hover:bg-blue-900 hover:text-white" onClick={() => createCheckoutSession('ONLINE BINARY CLASSES')}>Apply</button>
+                    <p className="p-2 text-sm font-semibold flex flex-grow items-center justify-center">Online binary classes in Deriv are educational resources designed to help traders learn and understand binary options trading in a digital format. These classes are accessible over the internet and offer a convenient way for individuals to acquire knowledge about various binary trading strategies, risk management techniques, and market analysis skills. Whether you're a beginner looking to get started or an experienced trader seeking to enhance your skills, Deriv's online binary classes provide valuable insights and training to help you make informed trading decisions in the binary options market.</p>
+                    <p className="p-3 text-lg font-semibold text-blue-900">KSH 15,000</p>
+                    <button className=" bg-blue-500 rounded-sm px-6 py-2 text-sm hover:bg-blue-900 hover:text-white" onClick={() => createCheckoutSession('DERIV ACCOUNT MANAGEMENT')}>Apply</button>
                     </div>
             </div>
 
